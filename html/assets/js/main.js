04390afc4cbd1ded36bf975dc92a8005d0bd9b3d
Sean <sean@tox.im>
2015-11-13 11:30:40 -0800

    might fix stats

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -185,13 +185,13 @@ if ('addEventListener' in document) {
 function newpage() {
     nanobar.go(100)
     $("html, body").animate({ scrollTop: 0 }, "fast");
-    if (location.hash.slice(1) == 'stats') {
-      setvar();
-    }
     if (location.hash == '') {
         $('#page-wrapper').load("index.html #page-wrapper");
     } else {
         $('#page-wrapper').load(location.hash.slice(1) + ".html #page-wrapper");
+        if (location.hash.slice(1) == 'stats') {
+          setvar();
+        }
     }
 };
 
@@ -346,7 +346,3 @@ function open_error_show() { //being lazy
         $("#errmsg").fadeIn();
         setTimeout(open_error_hide, 2000);
 }
-
-
-setvar(); //I'm going to regret this, kill me
-          //tl;dr: get and cache stats on first load
